2f25688afa2a60b76fb135617c54d6a55a29e942
Jonathan Feng <jonathanfeng5@gmail.com>
2025-11-06 10:38:48 -0500

    Time: 102 ms (54.75%) | Memory: 36.8 MB (45.25%) - LeetSync

--- 2483-task-scheduler-ii/task-scheduler-ii.py
+++ 2483-task-scheduler-ii/task-scheduler-ii.py
@@ -4,23 +4,26 @@ class Solution:
         taskHist = {} # task number -> day it was last done
 
         i = 0
-        days = 0
+        days = 1
 
         while i < len(tasks):
             tType = tasks[i]
 
-            if tType not in taskHist or days - taskHist[tType] > space:
-                days += 1
+            if tType not in taskHist:
                 taskHist[tType] = days
-                # print(i)
-            else: # we need to wait
+                days += 1
+            else:
                 timeWaited = days - taskHist[tType]
-                addWait = space - timeWaited + 1
-                
-                days += addWait
-                taskHist[tType] = days # update the history
-                # print(i, timeWaited, addWait, days)
+                if timeWaited > space: # immediatly do task
+                    taskHist[tType] = days
+                    days += 1
+                else: # wait until timeWaited is big enough
+                    # 3 - 1 -> 2, 2 = 3 - 2 + 1
+                    addWait = space - timeWaited + 1
+                    days += addWait
+                    taskHist[tType] = days # update the history
+                    days += 1 # get to the next day
             i += 1
 
-        return days
+        return days - 1
         
